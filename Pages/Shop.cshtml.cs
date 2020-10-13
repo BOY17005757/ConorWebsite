783b94b35f148487d25f2fd4ea80434ade2892f8
Conor Boyland <boy17005757@btc.ac.uk>
2020-10-13 10:23:20 +0100

    Add project files.

---
+++ Pages/Shop.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ConorWebsite.Pages
+{
+    public class ShopModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+
+        //public static string helloWorld()
+        //{
+        //    string test = "Hello World";
+
+        //    return test;
+        //}
+    }
+}
